e423d9c563dc0c64ba8cad4a060db3f0f1123ac4
daline da silva ribeiro <dalineribeiro@Daline-Ribeiro.local>
2026-04-26 08:57:57 -0300

    build: force redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// Build trigger: finalize-corpo-quente-v2
+// Build trigger: force-refresh-sunday
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
